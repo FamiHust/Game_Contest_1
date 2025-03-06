8d632f7dbca9ea1fa52b235ab37337715c5658b9
FamiHust <duc.vm224305@gmail.com>
2025-03-06 16:08:42 +0700

    update

--- Assets/Radar/Scripts/Radar.cs
+++ Assets/Radar/Scripts/Radar.cs
@@ -1,20 +1,8 @@
-﻿/* 
-    ------------------- Code Monkey -------------------
-    
-    Thank you for downloading the Code Monkey Utilities
-    I hope you find them useful in your projects
-    If you have any questions use the contact form
-    Cheers!
-
-               unitycodemonkey.com
-    --------------------------------------------------
- */
- 
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using CodeMonkey.Utils;
-using CodeMonkey;
+// using CodeMonkey;
 
 public class Radar : MonoBehaviour {
 
@@ -39,28 +27,18 @@ public class Radar : MonoBehaviour {
         float currentRotation = (sweepTransform.eulerAngles.z % 360) - 180;
 
         if (previousRotation < 0 && currentRotation >= 0) {
-            // Half rotation
             colliderList.Clear();
         }
 
         RaycastHit2D[] raycastHit2DArray = Physics2D.RaycastAll(transform.position, UtilsClass.GetVectorFromAngle(sweepTransform.eulerAngles.z), radarDistance, radarLayerMask);
         foreach (RaycastHit2D raycastHit2D in raycastHit2DArray) {
             if (raycastHit2D.collider != null) {
-                // Hit something
                 if (!colliderList.Contains(raycastHit2D.collider)) {
-                    // Hit this one for the first time
                     colliderList.Add(raycastHit2D.collider);
-                    //CMDebug.TextPopup("Ping!", raycastHit2D.point);
                     RadarPing radarPing = Instantiate(pfRadarPing, raycastHit2D.point, Quaternion.identity).GetComponent<RadarPing>();
-                    //radarPing.transform.SetParent(transform);
-                    if (raycastHit2D.collider.gameObject.GetComponent<ItemHandler>() != null) 
-                    {
-                        // Hit an Item
-                        radarPing.SetColor(new Color(0, 1, 0));
-                    }
+
                     if (raycastHit2D.collider.gameObject.GetComponent<EnemyController>() != null) 
                     {
-                        // Hit an Enemy
                         radarPing.SetColor(new Color(1, 0, 0));
                     }
                     radarPing.SetDisappearTimer(360f / rotationSpeed * 1f);
@@ -68,14 +46,14 @@ public class Radar : MonoBehaviour {
             }
         }
         
-        if (Input.GetKeyDown(KeyCode.T)) {
-            rotationSpeed += 20;
-            Debug.Log("rotationSpeed: " + rotationSpeed);
-        }
-        if (Input.GetKeyDown(KeyCode.R)) {
-            rotationSpeed -= 20;
-            Debug.Log("rotationSpeed: " + rotationSpeed);
-        }
+        // if (Input.GetKeyDown(KeyCode.T)) {
+        //     rotationSpeed += 20;
+        //     Debug.Log("rotationSpeed: " + rotationSpeed);
+        // }
+        // if (Input.GetKeyDown(KeyCode.R)) {
+        //     rotationSpeed -= 20;
+        //     Debug.Log("rotationSpeed: " + rotationSpeed);
+        // }
     }
 
 }
